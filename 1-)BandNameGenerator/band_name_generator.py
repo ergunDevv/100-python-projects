bc77d46dd1d7450ed2be94e8714a61696753d198
ergunDevv <ergncyln2127088@gmail.com>
2023-07-14 10:10:10 +0300

    band name generator and fundamentals of the python

---
+++ 1-)BandNameGenerator/band_name_generator.py
@@ -0,0 +1,9 @@
+print('Welcome to the Band Name Generator.')
+print("What's name of the city you grew up in?")
+# Getting the name of the city person grew up from user and save to the variable
+name_of_the_city_person_grow = input()
+# Getting the name of the person's pet from user and save to the variable
+print("What's your pet's name?")
+persons_pet_name = input()
+# Than printing to the console all.
+print('Your band name could be '+name_of_the_city_person_grow+' '+persons_pet_name)
---
+++ 1-)BandNameGenerator/print_input_len_functions_and_fundamentals.py
@@ -0,0 +1,15 @@
+# The print() function prints whatever is inside the parenthesis to the console.
+print('Hello World')
+# String concatenation can be done with a plus sign
+print('Hello ' + 'World!')
+
+# input() will get user input in console
+print('Hello ' + input('What is your name?') + '!')
+
+# len() function gets the string and returns the length of the string.
+print(len('Hello'))
+
+# In python variables can define directly(without any type. Python understands the what type of variable you define )
+# Like favNumber=5 or myDogsName='Nymeria'
+lengthOfTheString = len('Hello')
+print(lengthOfTheString)
